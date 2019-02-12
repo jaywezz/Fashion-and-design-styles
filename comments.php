ff2345d028cede050812afb9cebc95c4d76e22e6
unknown <jaywezz>
2019-02-12 20:30:14 +0000

    updated my project files

---
+++ comments.php
@@ -0,0 +1,64 @@
+<!-- Section: form gradient -->
+<section class="form-gradient mb-5 font_description>
+
+  <!--Form with header-->
+  <div class="card" style="w">
+
+    <!--Header-->
+    <div class="header peach-gradient">
+
+      <div class="row d-flex justify-content-center">
+        <h3 class="white-text mb-0 py-5 font-weight-bold">Contact Us</h3>
+      </div>
+
+    </div>
+    <!--Header-->
+
+    <div class="card-body mx-4">
+
+      <div class="md-form">
+        <i class="fas fa-user prefix grey-text"></i>
+        <input type="text" id="form104" class="form-control">
+        <label for="form104">Your name</label>
+      </div>
+
+      <div class="md-form">
+        <i class="fas fa-envelope prefix grey-text"></i>
+        <input type="text" id="form105" class="form-control">
+        <label for="form105">Your email</label>
+      </div>
+
+      <div class="md-form">
+        <i class="fas fa-tag prefix grey-text"></i>
+        <input type="text" id="form106" class="form-control">
+        <label for="form106">Subject</label>
+      </div>
+
+      <div class="md-form">
+        <i class="fas fa-pencil-alt prefix grey-text"></i>
+        <textarea type="text" id="form107" class="md-textarea form-control" rows="5"></textarea>
+        <label for="form107">Your message</label>
+      </div>
+
+
+      <!--Grid row-->
+      <div class="row d-flex align-items-center mb-3 mt-4">
+
+        <!--Grid column-->
+        <div class="col-md-12">
+          <div class="text-center">
+            <button type="button" class="btn btn-grey btn-rounded z-depth-1a">Send</button>
+          </div>
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+    </div>
+
+  </div>
+  <!--/Form with header-->
+
+</section>
+<!-- Section: form gradient -->
+<!-- Section: form dark -->
---
+++ footer.php
@@ -0,0 +1,151 @@
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="refresh.css">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<!-- Footer -->
+<footer style="padding-top: 50px; " class="page-footer font-small unique-color-dark footer-description">
+
+    <div style="background-color:  #e7a604 ">
+      <div class="container">
+
+        <!-- Grid row-->
+        <div class="row py-4 d-flex align-items-center">
+
+          <!-- Grid column -->
+          <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+            <h6 class="mb-0">We give the best dressing ideas in all occassions.</h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fab fa-facebook-f white-text mr-4"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fab fa-twitter white-text mr-4"> </i>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fab fa-google-plus-g white-text mr-4"> </i>
+            </a>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fab fa-linkedin-in white-text mr-4"> </i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fab fa-instagram white-text"> </i>
+            </a>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
+      </div>
+    </div>
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left mt-5">
+
+      <!-- Grid row -->
+      <div class="row mt-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+          <!-- Content -->
+          <h6 class="text-uppercase font-weight-bold">Company name</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            
+          Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit.</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Products</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">Sports code</a>
+          </p>
+          <p>
+            <a href="#!">Party code</a>
+          </p>
+          <p>
+            <a href="#!">Wedding Code</a>
+          </p>
+          <p>
+            <a href="#!">Best Code</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold"></h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">Your Account</a>
+          </p>
+          <p>
+            <a href="#!">Log Out</a>
+          </p>
+          <p>
+            <a href="#!">Sign Up</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Contact</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <i class="fas fa-home mr-3"></i>Kisumu, Kenya</p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i> wawerujaymo92@gmail.com.com</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          <p>
+            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/">@Ariyanastechies.All rights reserved</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
