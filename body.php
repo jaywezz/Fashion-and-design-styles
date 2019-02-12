23495be233f023872bc72b724a278118d20c0eeb
unknown <jaywezz>
2019-02-12 20:29:18 +0000

    updated my files

--- body.php
+++ body.php
@@ -1,19 +1,18 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="overlay.css">
 </head>
-<div class="row columns" >
+
+<div class="row columns font_description">
   <div class="col-sm-6 hovereffect">
-  	<img src="pin7.jpg">
-  	<div class="overlay">
-  		<h2 class="classy">CLASSY</h2>
-  		<a class="info" href="">SHOW MORE</a>
-  	</div>
+
+    <img src="pin50.jpg">
+    <div style="padding-top: 140px" class="overlay">
+     
+     
+    </div>
   </div>
   <div class="col-sm-6 font_details" ><p class="details ">Classy fashionable and irresistible modern styles</p>
-      <button type="button" class="btn btn-warning">Show Modern Outfits</button>
-    
+    <button type="button" class="btn btn-warning">Show Modern Outfits</button>
   </div>
-  <?php
-   echo "Hello world";
-  ?>
+  
 </div>
